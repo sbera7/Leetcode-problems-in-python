20522b92289d9d86a22e15b6afb6584b3ad71ca5
Shreyangshu <79297450+shreyangshu12@users.noreply.github.com>
2024-01-16 16:29:44 +0530

    Create 009_Palindrome_Number.py

---
+++ 009_Palindrome_Number.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x<0: return False
+        num = x
+        rev = 0
+        while num>0:
+            rev = (rev * 10) + num % 10
+            num = int(num / 10)
+        if rev == x: 
+            return True 
+        else: 
+            return False

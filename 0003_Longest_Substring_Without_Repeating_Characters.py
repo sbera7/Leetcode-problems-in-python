a2488f42d25a3f1f85ce9e431e0dcd35ce74890a
Shreyangshu <79297450+sbera7@users.noreply.github.com>
2024-03-20 16:47:57 +0530

    Update 0003_Longest_Substring_Without_Repeating_Characters.py

--- 0003_Longest_Substring_Without_Repeating_Characters.py
+++ 0003_Longest_Substring_Without_Repeating_Characters.py
@@ -1,21 +1,12 @@
 class Solution:
-    def lengthOfLongestSubstring(self, s: str) -> int:
-        left = 0 # pointer on which the repeatition is found
-        right = 0 # pointer for the current position
-        temp_list=[] # temporary list for every substring 
-        temp_dict={} # dictionary for storing the indices of the character
-        length = 0 # length of the longest substring
-
-        for c in s:
-            if c in temp_list:
-                left = temp_dict[c] + 1
-                temp_dict[c] = right # updating the indices if any repeatition is found
-                right = right + 1
-
-            else:
-                temp_dict[c] = right
-                right = right + 1
-            temp_list = s[left : right]
-            length = length if length > len(temp_list) else len(temp_list)
-            # print(temp_list)
-        return length
+    def lengthOfLongestSubstring(self, s) -> int:
+        l, r = 0, 1
+        res = 0
+        char_set = set()
+        for r in range(len(s)):
+            while s[r] in char_set:
+                char_set.remove(s[l])
+                l += 1
+            char_set.add(s[r])
+            res = max(res, r - l + 1)
+        return res

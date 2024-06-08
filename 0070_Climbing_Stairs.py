01c588cb95372e8a992653635b321b1918255d35
Shreyangshu <79297450+sbera7@users.noreply.github.com>
2024-06-08 14:25:15 +0530

    Update 0070_Climbing_Stairs.py

--- 0070_Climbing_Stairs.py
+++ 0070_Climbing_Stairs.py
@@ -1,1 +1,14 @@
-
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n == 1:
+            return 1
+        a, b = 1, 1
+        for i in range(n -1):
+            c = a + b
+            a = b
+            b = c
+        return c
